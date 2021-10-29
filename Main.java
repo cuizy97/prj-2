531ccff0882806e0e05b434673ee09652f0b42de
ZiYan Cui <89417144+cuizy97@users.noreply.github.com>
2021-10-29 18:29:27 +0000

    yay

--- Main.java
+++ Main.java
@@ -1,41 +1,34 @@
-/* @author: 
-@version: 
+/* @author: Dillon Pearon, Joseph Hannah, Wenhan Zhu, ZiYan Cui
+@version: 10/29/2021
 */
 import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    // Pillowcase object named bag:
-    Pillowcase bag = new Pillowcase();  
-    // we might need to create a Pillowcase object before
-    // calling methods from Pillowcase
-    // mentioned in No.3 first and second bullet points
-
-    // ask for user input:
     System.out.println("Happy Halloween!\nHow many houses should we trick or treat at?");
+    // Pillowcase object created:
+    Pillowcase bag = new Pillowcase();
+
+    // Ask to input total number of candies need to generate:
     Scanner s = new Scanner(System.in);
     int houseNum;
     houseNum=s.nextInt();
 
-    if(houseNum <= Pillowcase.size()){ // #### error on Pillowcase.size()
-      // Loop calling getCandy():
-      for (int i = 0; i< houseNum; i++){
-        bag.getCandy(); 
+    // When the input number <= max size of pillowcase, run the program:
+    if( houseNum <= bag.size){
+      // Loop for generate input number of candies from pillowcase:
+      for (int i = 0; i < houseNum; i++){
+        bag.getCandy();
       }
-      // use accessor method getNumber() divided by SIZE of Pillowcase:
-      int percentPillowcase = (bag.getNumber() / Pillowcase.size()) * 100;
-      double decimalPercent = percentPillowcase; 
-      // #### error on Pillowcase.size()
+      // Convert int to double:
+      double percentPillowcase = (bag.getNumber()  /(double) bag.size)* 100;
+      System.out.println("\nWe have " + bag.getNumber() +" candies, " + percentPillowcase +"% of our pillowcase.");
 
-      System.out.println("We have" + bag.getNumber() +" candies, " + percentPillowcase +"% of our pillowcase."); 
-      // should be decimalPercent???
-
+      // Print out the count and candy's name of those that generated:
       bag.printNumCandies();
     }
     else {
       System.out.println("Your pillowcase will not be able to hold all the candy you will receive from that many houses!");
     }
-
-  }
-    
+  }   
 }
--- Pillowcase.java
+++ Pillowcase.java
@@ -1,83 +1,75 @@
-/* @author: 
-@version: 
-*/
 import java.util.ArrayList;
 import java.util.Random;
 
 class Pillowcase {
-  private int candies;  // did we very use this?????
-  private int number;
-  int size = 1000;  // constant instance SIZE hold up to 1000 candies.
-  
-  
-  ArrayList<Candy> allCandy;
-
+  private int number;     // number of candy in the pillowcase
+  public final int size;  // constant instance hold up to 1000 candies.
+  ArrayList<Candy> allCandy;  // stores all the candies that randomly generated
+  int numCandy[] = {0, 0, 0, 0, 0, 0, 0};  // count of each candy
 
   // constructor initializing candies ArrayList.
   Pillowcase() {
+    size = 1000;
     number = 0; 
     allCandy = new ArrayList<Candy>();
   }
 
 
-  int numCandy[] = {0, 0, 0, 0, 0, 0, 0};
-  
-
   // method to getCandy()
   void getCandy(){
-    System.out.println("Trick or Treat!");
-
+    System.out.print("Trick or Treat!");
     Random r = new Random();
-    int rNum= r.nextInt(100) + 1;
+    int rNum= r.nextInt(100) + 1;  // randomly generate a number
 
     if (rNum < 6){          // 1-5, 5%
-      System.out.print(" We got Kit Kat!");
+      System.out.println(" We got Kit Kat!");
       number++;  
       Candy cd1 = new Candy("Kit Kat");
       allCandy.add(cd1);
     } 
     else if (rNum < 16){    // 6-15, 10%
-      System.out.print(" We got M&Ms!");
+      System.out.println(" We got M&Ms!");
       Candy cd2 = new Candy("M&Ms");
       allCandy.add(cd2);
       number ++;
     } 
     else if (rNum < 36){    // 16-35, 20%
-      System.out.print(" We got Pink Starburst!");
+      System.out.println(" We got Pink Starburst!");
       Candy cd3 = new Candy("Pink Starburst");
       allCandy.add(cd3);
       number ++; 
     }
     else if (rNum < 51){    // 36-50, 15%
-      System.out.print(" We got Hershey's Bar!");
+      System.out.println(" We got Hershey's Bar!");
       Candy cd4 = new Candy("Hershey's Bar");
       allCandy.add(cd4);
       number ++; 
     }
     else if (rNum < 61){   //51-60, 10%
-      System.out.print(" We got Milky Way!");
+      System.out.println(" We got Milky Way!");
       Candy cd5 = new Candy("Milky Way");
       allCandy.add(cd5);
       number ++; 
     }
     else if (rNum < 76){   //61-75, 15%
-      System.out.print(" We got Twix!");
+      System.out.println(" We got Twix!");
       Candy cd6 = new Candy("Twix");
       allCandy.add(cd6);
       number ++; 
     }
     else {                 //76-100, 25%
-      System.out.print(" We got Reese's!");
+      System.out.println(" We got Reese's!");
       Candy cd7 = new Candy("Reese's");
       allCandy.add(cd7); 
       number ++;
     }
   }
 
-
-
-  void  printNumCandies() {
+  
+  // method for getting how much of each candy that have gotten
+  void printNumCandies() {
     System.out.println("We got..." );
+    // store the count of candy that generated
     for (int i=0; i<allCandy.size(); i++){
       if (allCandy.get(i).getName().equals("Kit Kat")){
         numCandy[0] ++;
@@ -101,35 +93,33 @@ class Pillowcase {
         numCandy[6] ++;
       }
     }
-    
+    // Print out the count and candy's name of those that generated:
     if (numCandy[0] >= 1){
-      System.out.println(numCandy[0] + "Kit Kats");
+      System.out.println(numCandy[0] + " Kit Kats");
     }
-    else if (numCandy[1] >= 1){
-      System.out.println(numCandy[1] + "M&Ms");
+    if (numCandy[1] >= 1){
+      System.out.println(numCandy[1] + " M&Ms");
     }
-    else if (numCandy[2] >= 1){
-      System.out.println(numCandy[2] + "Pink Starburst");
+    if (numCandy[2] >= 1){
+      System.out.println(numCandy[2] + " Pink Starburst");
     }
-    else if (numCandy[3] >= 1){
-      System.out.println(numCandy[3] + "Hershey's Bar");
+    if (numCandy[3] >= 1){
+      System.out.println(numCandy[3] + " Hershey's Bar");
     }
-    else if (numCandy[4] >= 1){
-      System.out.println(numCandy[4] + "Milky Way");
+    if (numCandy[4] >= 1){
+      System.out.println(numCandy[4] + " Milky Way");
     }
-    else if (numCandy[5] >= 1){
-      System.out.println(numCandy[5] + "Twix");
+    if (numCandy[5] >= 1){
+      System.out.println(numCandy[5] + " Twix");
     }
-    else {
-      System.out.println(numCandy[6] + "Reese's");
+    if (numCandy[6] >= 1) {
+      System.out.println(numCandy[6] + " Reese's");
     }
   }
   
-
-
+  
   //accessor method to get number
-  int getNumber() {
-    return number;  // @return: it returns the total number of candy
+   int getNumber() {
+    return number; // @return: it returns the total number of candy
   }
-
 }
